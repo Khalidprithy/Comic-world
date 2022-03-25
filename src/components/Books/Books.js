3425e15b82aa4522331a342b2ff7b1ccf3e3452a
Khalidprithy <khalidprithy@gmail.com>
2022-03-25 21:59:48 +0600

    card button and icon added

--- src/components/Books/Books.js
+++ src/components/Books/Books.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { FaShoppingCart } from 'react-icons/fa';
 import './Book.css'
 
 const Books = ({ book }) => {
@@ -11,6 +12,11 @@ const Books = ({ book }) => {
             <div className='book-info'>
                 <h4>{name}</h4>
                 <p>Price: ${price}</p>
+            </div>
+            <div className='btn-cart-icon'>
+                <button className='cart-btn'>Add to Cart
+                    <FaShoppingCart className='icon'></FaShoppingCart>
+                </button>
             </div>
         </div>
     );
